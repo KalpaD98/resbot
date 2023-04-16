b6a43102b1a2eecef53e70b2c00386dd3cc48b57
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-04-16 23:52:23 +0530

    added exception handling for user_actions.py

--- actions/all_actions/user_actions.py
+++ actions/all_actions/user_actions.py
@@ -22,45 +22,49 @@ class ActionCompleteRegistration(Action):
             tracker: Tracker,
             domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        # check of user with mail already exists
+        try:
+            user_name = tracker.get_slot(USER_NAME)
+            user_email = tracker.get_slot(USER_EMAIL)
+            user_password = tracker.get_slot(USER_PASSWORD)
 
-        # set slots to None
+            existing_user = user_repo.get_user_by_email(user_email)
 
-        # Create a User and save them in the database
-        user_name = tracker.get_slot(USER_NAME)
-        user_email = tracker.get_slot(USER_EMAIL)
-        user_password = tracker.get_slot(USER_PASSWORD)
+            if existing_user:
+                dispatcher.utter_message(text="This email is already registered. Please log in.")
+                return [FollowupAction(ACTION_ASK_REGISTERED_AND_SHOW_LOGIN_SIGNUP_QUICK_REPLIES)]
 
-        user = User(user_name, user_email, user_password)
+            user = User(user_name, user_email, user_password)
+            user_id = user_repo.insert_user(user)
 
-        user_id = user_repo.insert_user(user)
+            if user_id is None:
+                dispatcher.utter_message(text="Registration failed. Please try again.")
+                return [FollowupAction(ACTION_ASK_REGISTERED_AND_SHOW_LOGIN_SIGNUP_QUICK_REPLIES)]
 
-        if user_id is None:
-            dispatcher.utter_message(
-                text="Registration failed. Please try again.")
-            return [FollowupAction(ACTION_ASK_REGISTERED_AND_SHOW_LOGIN_SIGNUP_QUICK_REPLIES)]
+            dispatcher.utter_message(text=f"Congratulations on completing your registration, {user.name}!")
+            dispatcher.utter_message(text="Your details are as follows:")
+            dispatcher.utter_message(text=f"Name: {user.name}")
+            dispatcher.utter_message(text=f"Email: {user.email}")
+            dispatcher.utter_message(text="You have been automatically logged in.")
 
-        # Set user details in the tracker
+            return [
+                SlotSet(LOGGED_USER, user),
+                SlotSet(USER_ID, user_id),
+                SlotSet(USER_NAME, user.name),
+                SlotSet(USER_EMAIL, user.email),
+                SlotSet(USER_PASSWORD, user.password),
+                SlotSet(IS_AUTHENTICATED, True)
+            ]
 
-        # Send a confirmation message to the user
-        dispatcher.utter_message(text="Congratulations on completing your registration " + user.name + "!")
-
-        # utter the details of the user
-        dispatcher.utter_message(text="Your details are as follows:")
-        dispatcher.utter_message(text="Name: " + user.name)
-        dispatcher.utter_message(text="Email: " + user.email)
-        dispatcher.utter_message(text="You have been automatically logged in.")
-        # TODO: quick reply to continue conversation
-        # dispatcher.utter_message(text="Password: " + ObjectUtils.star_print(len(user_password)))
-
-        return [
-            SlotSet(LOGGED_USER, user),
-            SlotSet(USER_ID, user_id),
-            SlotSet(USER_NAME, user.name),
-            SlotSet(USER_EMAIL, user.email),
-            SlotSet(USER_PASSWORD, user.password),
-            SlotSet(IS_AUTHENTICATED, True)
-        ]
+        except Exception as e:
+            logger.error(f"An error occurred in action_complete_registration: {e}")
+            dispatcher.utter_message(text="An error occurred. Please try again later.")
+            return [
+                SlotSet(LOGGED_USER, None),
+                SlotSet(USER_ID, None),
+                SlotSet(USER_NAME, None),
+                SlotSet(USER_EMAIL, None),
+                SlotSet(USER_PASSWORD, None),
+                SlotSet(IS_AUTHENTICATED, False)]
 
 
 class ActionLoginUser(Action):
@@ -73,65 +77,58 @@ class ActionLoginUser(Action):
             tracker: Tracker,
             domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        login_email = tracker.get_slot(USER_EMAIL)
-        login_password = tracker.get_slot(USER_PASSWORD)
+        try:
+            login_email = tracker.get_slot(USER_EMAIL)
+            login_password = tracker.get_slot(USER_PASSWORD)
 
-        user = user_repo.find_user_by_email(login_email)
+            user = user_repo.find_user_by_email(login_email)
 
-        if user is None:
-            dispatcher.utter_message(
-                text="User with the email provided does not exist. Please try again with a different email.")
-            return [
-                SlotSet(LOGGED_USER, None),
-                SlotSet(USER_NAME, None),
-                SlotSet(USER_ID, None),
-                SlotSet(USER_EMAIL, None),
-                SlotSet(USER_PASSWORD, None),
-                SlotSet(IS_AUTHENTICATED, False),
-                FollowupAction(ACTION_RETRY_LOGIN_OR_STOP)
-            ]
-        # authenticate the user
-        if user.password == login_password:
-            print(user)
-            quick_replies_with_payload = []
+            if user is None:
+                dispatcher.utter_message(
+                    text="User with the email provided does not exist. Please try again with a different email.")
+                return [
+                    SlotSet(LOGGED_USER, None),
+                    SlotSet(USER_NAME, None),
+                    SlotSet(USER_ID, None),
+                    SlotSet(USER_EMAIL, None),
+                    SlotSet(USER_PASSWORD, None),
+                    SlotSet(IS_AUTHENTICATED, False),
+                    FollowupAction(ACTION_RETRY_LOGIN_OR_STOP)
+                ]
 
-            quick_reply_request_restaurant = {
-                TITLE: "Checkout restaurants",
-                PAYLOAD: "/request_restaurants"}
+            if user.password == login_password:
+                quick_replies_with_payload = [
+                    {"title": "Checkout restaurants", "payload": "/request_restaurants"},
+                    {"title": "Search restaurants", "payload": "/search_restaurants"},
+                    {"title": "View bookings", "payload": "/view_bookings"}
+                ]
 
-            quick_reply_search_restaurants = {
-                TITLE: "Search restaurants",
-                PAYLOAD: "/search_restaurants"}
+                dispatcher.utter_message(response="utter_login_success",
+                                         quick_replies=ResponseGenerator.quick_replies(quick_replies_with_payload,
+                                                                                       True))
 
-            quick_reply_view_bookings = {
-                TITLE: "View bookings",
-                PAYLOAD: "/view_bookings"}
+                return [
+                    SlotSet(LOGGED_USER, user.to_dict()),
+                    SlotSet(USER_NAME, user.name),
+                    SlotSet(USER_ID, user.id),
+                    SlotSet(USER_EMAIL, user.email),
+                    SlotSet(USER_PASSWORD, user.password),
+                    SlotSet(IS_AUTHENTICATED, True),
+                ]
 
-            quick_replies_with_payload.append(quick_reply_request_restaurant)
-            quick_replies_with_payload.append(quick_reply_search_restaurants)
-            quick_replies_with_payload.append(quick_reply_view_bookings)
-
-            dispatcher.utter_message(response="utter_login_success",
-                                     quick_replies=ResponseGenerator.quick_replies(quick_replies_with_payload, True))
-
-            return [
-                SlotSet(LOGGED_USER, user.to_dict()),
-                SlotSet(USER_NAME, user.name),
-                SlotSet(USER_ID, user.id),
-                SlotSet(USER_EMAIL, user.email),
-                SlotSet(USER_PASSWORD, user.password),
-                SlotSet(IS_AUTHENTICATED, True),
-            ]
-
-        else:
-            dispatcher.utter_message(text="password is incorrect.")
-            return [
-                SlotSet(LOGGED_USER, None),
-                SlotSet(USER_NAME, None),
-                SlotSet(USER_ID, None),
-                SlotSet(USER_EMAIL, None),
-                FollowupAction(ACTION_RETRY_LOGIN_OR_STOP)
-            ]
+            else:
+                dispatcher.utter_message(text="Incorrect password.")
+                return [
+                    SlotSet(LOGGED_USER, None),
+                    SlotSet(USER_NAME, None),
+                    SlotSet(USER_ID, None),
+                    SlotSet(USER_EMAIL, None),
+                    FollowupAction(ACTION_RETRY_LOGIN_OR_STOP)
+                ]
+        except Exception as e:
+            logger.error(f"An error occurred in action_login_user: {e}")
+            dispatcher.utter_message(text="An error occurred. Please try again later.")
+            return []
 
 
 class ActionRetryLoginOrStop(Action):
@@ -141,23 +138,16 @@ class ActionRetryLoginOrStop(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        quick_replies_with_payload = []
+        try:
 
-        quick_reply_retry = {
-            TITLE: "Yes",
-            PAYLOAD: "/request_login_form"}
+            dispatcher.utter_message(text="Would you like to retry logging in?",
+                                     quick_replies=ResponseGenerator.quick_reply_yes_no_with_payload())
 
-        quick_reply_stop = {
-            TITLE: "No",
-            PAYLOAD: "/stop"}
-
-        quick_replies_with_payload.append(quick_reply_retry)
-        quick_replies_with_payload.append(quick_reply_stop)
-
-        dispatcher.utter_message(text="Would you like to retry logging in?",
-                                 quick_replies=ResponseGenerator.quick_replies(quick_replies_with_payload, True))
-
-        return []
+            return []
+        except Exception as e:
+            logger.error(f"An error occurred in action_retry_login_or_stop: {e}")
+            dispatcher.utter_message(text="An error occurred. Please try again later.")
+            return []
 
 
 class ActionAskRegisteredAndShowLoginSignupQuickReplies(Action):
@@ -167,21 +157,26 @@ class ActionAskRegisteredAndShowLoginSignupQuickReplies(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # Define quick replies
-        quick_replies_list = [
-            {
-                TITLE: "Login",
-                PAYLOAD: "/request_login_form"
-            },
-            {
-                TITLE: "Register",
-                PAYLOAD: "/request_user_registration_form"
-            }
-        ]
+        try:
+            # Define quick replies
+            quick_replies_list = [
+                {
+                    TITLE: "Login",
+                    PAYLOAD: "/request_login_form"
+                },
+                {
+                    TITLE: "Register",
+                    PAYLOAD: "/request_user_registration_form"
+                }
+            ]
 
-        # Generate quick replies using the ResponseGenerator class
-        quick_replies = ResponseGenerator.quick_replies(quick_replies_list, with_payload=True)
-        dispatcher.utter_message(text="Please log in or sign up to continue.", quick_replies=quick_replies)
+            # Generate quick replies using the ResponseGenerator class
+            quick_replies = ResponseGenerator.quick_replies(quick_replies_list, with_payload=True)
+            dispatcher.utter_message(text="Please log in or sign up to continue.", quick_replies=quick_replies)
+
+        except Exception as e:
+            logger.error(f"An error occurred in action_ask_registered_and_show_login_signup_quick_replies: {e}")
+            dispatcher.utter_message(text="An error occurred. Please try again later.")
 
         return []
 
@@ -191,11 +186,16 @@ class ActionLogout(Action):
         return ACTION_LOGOUT
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        return [
-            SlotSet("logged_user", None),
-            SlotSet("user_name", None),
-            SlotSet("user_id", None),
-            SlotSet("user_email", None),
-            SlotSet("password", None),
-            SlotSet(IS_AUTHENTICATED, False),
-        ]
+        try:
+            return [
+                SlotSet("logged_user", None),
+                SlotSet("user_name", None),
+                SlotSet("user_id", None),
+                SlotSet("user_email", None),
+                SlotSet("password", None),
+                SlotSet(IS_AUTHENTICATED, False),
+            ]
+        except Exception as e:
+            logger.error(f"An error occurred in action_logout: {e}")
+            dispatcher.utter_message(text="An error occurred. Please try again later.")
+            return []
