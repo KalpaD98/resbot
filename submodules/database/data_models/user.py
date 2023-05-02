b2e7dbb549085f2424626f2b38420858a2464961
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-05-03 00:32:31 +0530

    added favorite_cuisines to user.py

--- submodules/database/data_models/user.py
+++ submodules/database/data_models/user.py
@@ -1,4 +1,5 @@
-# user.py
+from typing import List, Optional
+
 from bson.objectid import ObjectId
 
 
@@ -7,18 +8,21 @@ class User:
     NAME = "name"
     EMAIL = "email"
     PASSWORD = "password"
+    FAVORITE_CUISINES = "favorite_cuisines"
 
-    def __init__(self, name: str, email: str, password: str, user_id: str = None):
+    def __init__(self, name: str, email: str, password: str, user_id: str = None,
+                 favorite_cuisines: Optional[List[str]] = None):
         self.id = user_id if user_id else f"uid_{ObjectId()}"
         self.name = name
         self.email = email
         self.password = password
+        self.favorite_cuisines = favorite_cuisines or []
 
     def __repr__(self) -> str:
-        return f"User(id='{self.id}', name='{self.name}', email='{self.email}', password='{self.password}')"
+        return f"User(id='{self.id}', name='{self.name}', email='{self.email}', password='{self.password}', favorite_cuisines={self.favorite_cuisines})"
 
     def __str__(self) -> str:
-        return f"User ID: {self.id}, Name: {self.name}, Email: {self.email}"
+        return f"User ID: {self.id}, Name: {self.name}, Email: {self.email}, Favorite Cuisines: {', '.join(self.favorite_cuisines)}"
 
     def to_dict(self):
         return {
@@ -26,6 +30,7 @@ class User:
             User.NAME: self.name,
             User.EMAIL: self.email,
             User.PASSWORD: self.password,
+            User.FAVORITE_CUISINES: self.favorite_cuisines,
         }
 
     @classmethod
@@ -35,4 +40,5 @@ class User:
             email=data[User.EMAIL],
             password=data[User.PASSWORD],
             user_id=data[User.ID],
+            favorite_cuisines=data.get(User.FAVORITE_CUISINES),
         )
