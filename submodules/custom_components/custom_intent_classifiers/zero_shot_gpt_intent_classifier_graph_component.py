9d39dd9f35551d26472ab0c4923903cd5c2d01c9
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-05-23 17:08:52 +0530

    updated zero_shot_gpt_intent_classifier.py need to do testing

--- submodules/custom_components/custom_intent_classifiers/zero_shot_gpt_intent_classifier_graph_component.py
+++ submodules/custom_components/custom_intent_classifiers/zero_shot_gpt_intent_classifier_graph_component.py
@@ -1,17 +1,18 @@
 import json
 import os
-from typing import Dict, Text, Any, List, Optional
+from typing import Dict, Text, Any, List, Tuple, Type
 
 from dotenv import load_dotenv
 from rasa.engine.graph import GraphComponent, ExecutionContext
 from rasa.engine.recipes.default_recipe import DefaultV1Recipe
 from rasa.engine.storage.resource import Resource
 from rasa.engine.storage.storage import ModelStorage
-from rasa.shared.nlu.constants import INTENT
+from rasa.nlu.classifiers.classifier import IntentClassifier
+from rasa.shared.nlu.constants import INTENT, TEXT
 from rasa.shared.nlu.training_data.message import Message
 from rasa.shared.nlu.training_data.training_data import TrainingData
+from skllm import ZeroShotGPTClassifier
 from skllm.config import SKLLMConfig
-from skllm.models.gpt_zero_shot_clf import ZeroShotGPTClassifier
 
 load_dotenv()
 
@@ -19,13 +20,31 @@ load_dotenv()
 @DefaultV1Recipe.register(
     DefaultV1Recipe.ComponentType.INTENT_CLASSIFIER, is_trainable=True
 )
-class ZeroShotGPTIntentClassifier(GraphComponent):
-    def __init__(self, model_storage: ModelStorage, resource: Resource, training_artifact: Optional[Dict] = None,
-                 ) -> None:
+class ZeroShotGPTIntentClassifier(IntentClassifier, GraphComponent):
+    @classmethod
+    def required_components(cls) -> List[Type]:
+        return []
+
+    @classmethod
+    def required_packages(cls) -> List[Text]:
+        """Any extra python dependencies required for this component to run."""
+        return ["skllm"]
+
+    @staticmethod
+    def get_default_config() -> Dict[Text, Any]:
+        return {}
+
+    @classmethod
+    def validate_config(cls, config: Dict[Text, Any]) -> None:
+        """Validates that the component is configured properly."""
+        pass
+
+    def __init__(self, config: Dict[Text, Any], name: Text, model_storage: ModelStorage, resource: Resource) -> None:
         print("init ZeroShotGPTIntentClassifier")
         self._model_storage = model_storage
         self._resource = resource
-        self.clf = ZeroShotGPTClassifier()  # initialize the classifier here
+        self.name = name
+        self.clf = ZeroShotGPTClassifier()
 
     @classmethod
     def create(
@@ -44,7 +63,7 @@ class ZeroShotGPTIntentClassifier(GraphComponent):
         SKLLMConfig.set_openai_key(openai_key)
         SKLLMConfig.set_openai_org(openai_organization)
         print("create ZeroShotGPTIntentClassifier")
-        return cls(model_storage, resource)
+        return cls(config, execution_context.node_name, model_storage, resource)
 
     @classmethod
     def load(
@@ -55,50 +74,47 @@ class ZeroShotGPTIntentClassifier(GraphComponent):
             execution_context: ExecutionContext,
             **kwargs: Any,
     ) -> GraphComponent:
-        # Load persisted classifier
+        print("Loading ZeroShotGPTIntentClassifier")
         try:
             with model_storage.read_from(resource) as directory_path:
-                with open(directory_path / "intents.json", "r") as file:
-                    intents = json.load(file)
-                    print("json.load(file) ZeroShotGPTIntentClassifier")
-                    return cls(model_storage, resource, intents)
-
+                intents = json.load(open(directory_path / "gpt_intents.json", "r"))
+                component = cls(config, execution_context.node_name, model_storage, resource)
+                component.clf.fit(None, intents)
+                print("Loaded ZeroShotGPTIntentClassifier")
+                return component
         except ValueError:
-            # No persisted data found for the given resource
-            return cls(model_storage, resource)
+            print("No data found for the given resource")
+            return cls(config, execution_context.node_name, model_storage, resource)
 
-    def train(self, training_data: TrainingData) -> Resource:
-        print("Training ZeroShotGPTIntentClassifier")
-        # Extract intents from training data
-        intents = list(set([example.get(INTENT) for example in training_data.training_examples]))
+    def _get_training_data(self, training_data: TrainingData) -> Tuple[List[Text], List[Text]]:
+        """Retrieve the text messages and their corresponding intents."""
+        print("Getting training data for ZeroShotGPTIntentClassifier")
+        messages = [example.get(TEXT) for example in training_data.training_examples if example.get(TEXT)]
+        print("messages: ", messages)
+        intents = [example.get(INTENT) for example in training_data.training_examples if example.get(INTENT)]
+        print("intents: ", intents)
+        return messages, intents
 
-        # Fit ZeroShotGPTClassifier
-        self.clf.fit(None, intents)
-
-        # Persist the classifier
-        with self._model_storage.write_to(self._resource) as directory_path:
-            with open(directory_path / "intents.json", "w") as file:
-                json.dump(intents, file)
-        print("Training ZeroShotGPTIntentClassifier complete")
-        return self._resource
+    def process_training_data(self, training_data: TrainingData) -> None:
+        """Preprocess the training data."""
+        print("Processing training data for ZeroShotGPTIntentClassifier")
+        messages, intents = self._get_training_data(training_data)
+        self.clf.fit(messages, intents)
 
     def process(self, messages: List[Message]) -> List[Message]:
+        """Process the list of messages."""
+        print("Processing ZeroShotGPTIntentClassifier")
         for message in messages:
-            print("Start processing using ZeroShotGPTIntentClassifier")
-            if message.data.get(INTENT) is None:
-                # Only classify intent if it's not available yet in the message
-                intents = self.clf.predict([message.get("text")])
-                print("intents: ", intents if intents is not None else "None")
-                message.data[INTENT] = {
-                    "name": intents[0],
-                    "confidence": 0.6,
-                    # you might need to handle confidence in a different way
-                }
-            else:
-                print("INTENT already exists in message")
-                print(message.get("text") if message.get("text") is not None else "no message")
-                # intents = self.clf.predict([message.get("text")])
-                print("Predicted intents by ZeroShotGPTIntentClassifier: ")
-                # print("intents: ", intents if intents is not None else "None")
-            print("Processing using ZeroShotGPTIntentClassifier ended")
+            text = message.get(TEXT)
+            if text:
+                prediction = self.clf.predict([text])
+                if prediction:
+                    message.set(INTENT, {"name": prediction[0], "confidence": 1.0})
+
         return messages
+
+    def persist(self) -> None:
+        print("Persisting ZeroShotGPTIntentClassifier")
+        intents = self.clf.classes_.tolist()
+        with self._model_storage.write_to(self._resource) as directory_path:
+            json.dump(intents, open(directory_path / "gpt_intents.json", "w"))
