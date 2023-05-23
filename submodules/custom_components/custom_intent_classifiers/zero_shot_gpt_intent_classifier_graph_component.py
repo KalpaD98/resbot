53b55b30a96ba4d88d6b3a2e2314621f01937f25
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-05-23 19:43:51 +0530

    did some bug fixing on zero_shot_gpt_intent_classifier_graph_component.py need to do further testing. time out http request.

--- submodules/custom_components/custom_intent_classifiers/zero_shot_gpt_intent_classifier_graph_component.py
+++ submodules/custom_components/custom_intent_classifiers/zero_shot_gpt_intent_classifier_graph_component.py
@@ -1,8 +1,9 @@
-import json
+import logging
 import os
 from typing import Dict, Text, Any, List, Type, Tuple
 
 from dotenv import load_dotenv
+from joblib import dump, load
 from rasa.engine.graph import GraphComponent, ExecutionContext
 from rasa.engine.recipes.default_recipe import DefaultV1Recipe
 from rasa.engine.storage.resource import Resource
@@ -14,6 +15,7 @@ from skllm import ZeroShotGPTClassifier
 from skllm.config import SKLLMConfig
 
 load_dotenv()
+logger = logging.getLogger(__name__)
 
 
 @DefaultV1Recipe.register(
@@ -40,10 +42,20 @@ class ZeroShotGPTIntentClassifier(GraphComponent):
 
     def __init__(self, config: Dict[Text, Any], name: Text, model_storage: ModelStorage, resource: Resource) -> None:
         print("init ZeroShotGPTIntentClassifier")
+        self.name = name
+
+        # Fetch the OpenAI key and organization from environment variables
+        openai_key = os.getenv("OPENAI_KEY")
+        openai_organization = os.getenv("OPENAI_ORGANIZATION")
+
+        # Set the OpenAI key and organization for SKLLMConfig
+        SKLLMConfig.set_openai_key(openai_key)
+        SKLLMConfig.set_openai_org(openai_organization)
+
+        self.clf = ZeroShotGPTClassifier()
+
         self._model_storage = model_storage
         self._resource = resource
-        self.name = name
-        self.clf = ZeroShotGPTClassifier()
 
     @classmethod
     def create(
@@ -54,15 +66,41 @@ class ZeroShotGPTIntentClassifier(GraphComponent):
             execution_context: ExecutionContext,
     ) -> GraphComponent:
 
-        # Fetch the OpenAI key and organization from environment variables
-        openai_key = os.getenv("OPENAI_KEY")
-        openai_organization = os.getenv("OPENAI_ORGANIZATION")
-
-        # Set the OpenAI key and organization for SKLLMConfig
-        SKLLMConfig.set_openai_key(openai_key)
-        SKLLMConfig.set_openai_org(openai_organization)
-        print("create ZeroShotGPTIntentClassifier")
         return cls(config, execution_context.node_name, model_storage, resource)
+
+    @staticmethod
+    def _get_training_data(training_data: TrainingData) -> Tuple[List[Text], List[Text]]:
+        """Preprocess the training data. Retrieve the text messages and their corresponding intents."""
+        print("Getting training data for ZeroShotGPTIntentClassifier")
+        messages = [example.get(TEXT) for example in training_data.training_examples if example.get(TEXT)]
+        print("messages: ", messages[:3])
+        intents = [example.get(INTENT) for example in training_data.training_examples if example.get(INTENT)]
+        print("intents: ", intents[:3])
+        return messages, intents
+
+    def train(self, training_data: TrainingData) -> Resource:
+
+        print("Training ZeroShotGPTIntentClassifier")
+        messages, intents = self._get_training_data(training_data)
+        if len(messages) == 0:
+            logger.debug(
+                f"Cannot train '{self.__class__.__name__}'. No data was provided. "
+                f"Skipping training of the classifier."
+            )
+        self.clf.fit(messages, intents)
+        print("Trained ZeroShotGPTIntentClassifier")
+        self.persist()
+        return self._resource
+
+    def persist(self) -> None:
+        """Persist the intents into model storage."""
+        print("Persisting ZeroShotGPTIntentClassifier")
+        with self._model_storage.write_to(self._resource) as model_dir:
+            dump(self.clf, model_dir / f"{self.name}.joblib")
+            print("Persisted ZeroShotGPTIntentClassifier")
+        # with self._model_storage.write_to(self._resource) as directory_path:
+        #     json.dump(intents, open(directory_path / "gpt_intents.json", "w"))
+        #     print("Persisted ZeroShotGPTIntentClassifier")
 
     @classmethod
     def load(
@@ -75,40 +113,24 @@ class ZeroShotGPTIntentClassifier(GraphComponent):
     ) -> GraphComponent:
         print("Loading ZeroShotGPTIntentClassifier")
         try:
-            with model_storage.read_from(resource) as directory_path:
-                intents = json.load(open(directory_path / "gpt_intents.json", "r"))
-                component = cls(config, execution_context.node_name, model_storage, resource)
-                component.clf.fit(None, intents)
+            # with model_storage.read_from(resource) as directory_path:
+            #     intents = json.load(open(directory_path / "gpt_intents.json", "r"))
+            #     component = cls(config, execution_context.node_name, model_storage, resource)
+            #     component.clf.fit(None, intents)
+            #     print("Loaded ZeroShotGPTIntentClassifier")
+            #     return component
+            with model_storage.read_from(resource) as model_dir:
+                classifier = load(model_dir / f"{resource.name}.joblib")
+                component = cls(
+                    config, execution_context.node_name, model_storage, resource
+                )
+                component.clf = classifier
                 print("Loaded ZeroShotGPTIntentClassifier")
                 return component
         except ValueError:
             print("Failed to load: No data found for the given resource")
             # TODO: In this case can get data from training and add here
             return cls(config, execution_context.node_name, model_storage, resource)
-
-    def _get_training_data(self, training_data: TrainingData) -> Tuple[List[Text], List[Text]]:
-        """Preprocess the training data. Retrieve the text messages and their corresponding intents."""
-        print("Getting training data for ZeroShotGPTIntentClassifier")
-        messages = [example.get(TEXT) for example in training_data.training_examples if example.get(TEXT)]
-        print("messages: ", messages[:5])
-        intents = [example.get(INTENT) for example in training_data.training_examples if example.get(INTENT)]
-        print("intents: ", intents[:5])
-        return messages, intents
-
-    def train(self, training_data: TrainingData) -> Resource:
-
-        print("Training ZeroShotGPTIntentClassifier")
-        messages, intents = self._get_training_data(training_data)
-        self.clf.fit(messages, intents)
-
-        return self._resource
-
-    def persist(self, intents: List[Text]) -> None:
-        """Persist the intents into model storage."""
-        print("Persist ZeroShotGPTIntentClassifier")
-        with self._model_storage.write_to(self._resource) as directory_path:
-            json.dump(intents, open(directory_path / "gpt_intents.json", "w"))
-            print("Persisted ZeroShotGPTIntentClassifier")
 
     def process(self, messages: List[Message]) -> List[Message]:
         """Process the list of messages."""
