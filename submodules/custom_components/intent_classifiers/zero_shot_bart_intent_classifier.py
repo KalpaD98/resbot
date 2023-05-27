7754693532829c07581d66d4d23c8fa24b139d50
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-05-27 23:03:18 +0530

    added ambiguity for zero_shot_bart_intent_classifier.py

--- submodules/custom_components/intent_classifiers/zero_shot_bart_intent_classifier.py
+++ submodules/custom_components/intent_classifiers/zero_shot_bart_intent_classifier.py
@@ -32,7 +32,13 @@ class ZeroShotBartIntentClassifier(GraphComponent):
 
     @staticmethod
     def get_default_config() -> Dict[Text, Any]:
-        return {"candidate_class_size": 5, "fallback_classifier_threshold": 0.3, "threshold": 0.2}
+        return {
+            "threshold": 0.2,
+            "ambiguity_threshold": 0.05,
+            "candidate_class_size": 5,
+            "fallback_classifier_threshold": 0.3,
+            "fallback_classifier_ambiguity_threshold": 0.1,
+        }
 
     @classmethod
     def validate_config(cls, config: Dict[Text, Any]) -> None:
@@ -42,12 +48,18 @@ class ZeroShotBartIntentClassifier(GraphComponent):
     def __init__(self, config: Dict[Text, Any], name: Text, model_storage: ModelStorage, resource: Resource) -> None:
         print("init ZeroShotBartIntentClassifier")
         self.name = name
+        print("Init ZeroShotBartIntentClassifier and model")
         self.clf = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
         self._model_storage = model_storage
         self._resource = resource
-        self.candidate_class_size = config["candidate_class_size"]
+
         self.threshold = config["threshold"]
+        self.ambiguity_threshold = config["ambiguity_threshold"]
+
+        self.candidate_class_size = config["candidate_class_size"]
+
         self.fallback_classifier_threshold = config["fallback_classifier_threshold"]
+        self.fallback_classifier_ambiguity_threshold = config["fallback_classifier_ambiguity_threshold"]
 
     @classmethod
     def create(
@@ -56,13 +68,12 @@ class ZeroShotBartIntentClassifier(GraphComponent):
             model_storage: ModelStorage,
             resource: Resource,
             execution_context: ExecutionContext,
-    ) -> GraphComponent:
+    ) -> 'ZeroShotBartIntentClassifier':
         return cls(config, execution_context.node_name, model_storage, resource)
 
     def process(self, messages: List[Message]) -> List[Message]:
         """Process the list of messages."""
-        # TODO: predict only if DIET fails
-        # check if nlu_fallback
+        # predict only if DIET fails
         print("Processing ZeroShotBartIntentClassifier")
         for message in messages:
             intent_ranking = message.get(INTENT_RANKING_KEY)
@@ -75,34 +86,31 @@ class ZeroShotBartIntentClassifier(GraphComponent):
             print("Process ZERO SHOT Bart Classifier complete")
             text = message.get(TEXT)
             # check for intent being nlu_fallback if yes then run below code
-            if text and intent['confidence'] <= 0.6:  # can use this condition to control
+            # can use this condition to control
+            if self._should_classify_with_bart(self, text, intent_ranking):
                 try:
                     # get from DIET and add from GPT pretrain and add other
-                    candidate_labels = self.get_candidate_labels(self, intent_ranking)
+                    candidate_labels = self._get_candidate_labels(self, intent_ranking)
                     print(text)
-
                     prediction_data = self.clf(text, candidate_labels)
                     # Transform to intent ranking format of rasa
-                    new_intent_ranking = self.get_intent_ranking(prediction_data)
+                    new_intent_ranking = self._get_bart_intent_ranking(prediction_data)
                     print("New intent rankings by bart zero shot")
                     print(new_intent_ranking)
-                    if len(new_intent_ranking) > 0:
-                        if new_intent_ranking[0]['name'] != 'other':  # if  new_intent_ranking[0] is other don't change
-                            # remove 'other'
-                            # Add as the new intent ranking
-                            # TODO: consider threshold (zero intent threshold) for new intent
-                            #  Approach: will have to increase to fbc value and reduce from other intents
-                            #  (in weighted manner)
-                            message.set(INTENT, new_intent_ranking[0], add_to_output=True)
-                            message.set(INTENT_RANKING_KEY, new_intent_ranking, add_to_output=True)
-                            print("prediction of zero shot: ", new_intent_ranking[0])
 
-                # TODO: when 'haiya chatbot' is entered greet is not in intent ranking so still fail
-                # TODO: when 'hiaya chatbot' is entered greet is in intent ranking
-                #  however after sending it to ZSC confidence is < than fallback_classifier_threshold hence trigger nlu_fallback
+                    if self._should_add_to_tracker(self, new_intent_ranking):
+                        message.set(INTENT, new_intent_ranking[0], add_to_output=True)
+                        message.set(INTENT_RANKING_KEY, new_intent_ranking, add_to_output=True)
+                        print("prediction of zero shot: ", new_intent_ranking[0])
+                    else:
+                        print("Not adding to tracker: not enough confidence")
+# TODO: when 'haiya chatbot' is entered greet is not in intent ranking so still fail
+# TODO: when 'hiaya chatbot' is entered greet is in intent ranking
+#  however after sending it to ZSC confidence is < than fallback_classifier_threshold hence trigger nlu_fallback
+
                 # TODO: if confidence is less than a certain value ask from user
                 #             # Set the language entity
-                #             # removed below for bug fixing TODO: uncomment for further debugging and improvements
+                #             # removed below for bug fixing
                 #             entity = {
                 #                 'value': language,
                 #                 'confidence': 0.1,
@@ -130,34 +138,64 @@ class ZeroShotBartIntentClassifier(GraphComponent):
         return unique_intents
 
     @staticmethod
-    def get_intent_ranking(prediction_data):
-        # Remove 'other' label and its corresponding score
-        if 'other' in prediction_data['labels']:
-            index = prediction_data['labels'].index('other')
-            del prediction_data['labels'][index]
-            del prediction_data['scores'][index]
+    def _should_classify_with_bart(self, text, intent_ranking):
+        """Determines whether text should be classified with BART classifier based on intent ranking and thresholds."""
+
+        # Make sure there are at least two intents in the intent_ranking
+        if len(intent_ranking) < 2:
+            return False
+
+        top_intent = intent_ranking[0]
+        return (
+                text and
+                top_intent['confidence'] < self.fallback_classifier_threshold and
+                (top_intent['confidence'] - intent_ranking[1]['confidence']) <
+                self.fallback_classifier_ambiguity_threshold
+        )
+
+    @staticmethod
+    def _should_add_to_tracker(self, bart_intent_ranking):
+        """Determines whether text should be processed with BART classifier based on intent ranking and thresholds."""
+        top_intent = bart_intent_ranking[0]
+        meet_threshold_confidence = top_intent['confidence'] >= self.threshold
+        if len(bart_intent_ranking) < 2:
+            return meet_threshold_confidence
+        else:
+            second_top_intent = bart_intent_ranking[1]
+            diff = top_intent['confidence'] - second_top_intent['confidence']
+            meet_ambiguity_threshold_confidence = diff >= self.ambiguity_threshold
+            return meet_threshold_confidence and meet_ambiguity_threshold_confidence
+
+    @staticmethod
+    def _get_bart_intent_ranking(prediction_data):
+        """Transform the prediction data into the intent ranking format of rasa."""
 
         # Create a list of tuples where each tuple is (label, score)
         label_score_pairs = list(zip(prediction_data['labels'], prediction_data['scores']))
 
         # Check the first label
-        if label_score_pairs[0][1] > 0.9:
-            selected_labels = [{'name': label_score_pairs[0][0], 'confidence': label_score_pairs[0][1]}]
+        if label_score_pairs[0][1] >= 0.9:
+            new_intent_ranking = [{'name': label_score_pairs[0][0], 'confidence': label_score_pairs[0][1]}]
         else:
             # Pick the highest scoring labels that sum up to at most 0.9
-            selected_labels = []
+            new_intent_ranking = []
             total_score = 0
             for label, score in label_score_pairs:
                 if total_score + score <= 0.99:
-                    selected_labels.append({'name': label, 'confidence': score})
+                    new_intent_ranking.append({'name': label, 'confidence': score})
                     total_score += score
                 else:
                     break
+        if new_intent_ranking[0]['name'] == 'other':
+            new_intent_ranking = []
+        else:
+            # Remove other
+            new_intent_ranking = [intent for intent in new_intent_ranking if intent['name'] != 'other']
 
-        return selected_labels
+        return new_intent_ranking
 
     @staticmethod
-    def get_candidate_labels(self, intent_ranking):
+    def _get_candidate_labels(self, intent_ranking):
         num_intents = len(intent_ranking)
         clas_size = self.candidate_class_size
         num_intents_to_classify = num_intents if num_intents < clas_size else clas_size  # five top intents
@@ -165,59 +203,61 @@ class ZeroShotBartIntentClassifier(GraphComponent):
         candidate_labels.append('other')  # to catch out of scope
         return candidate_labels
 
-    @staticmethod
-    def compute_weighted_average(diet_intent_ranking, zero_shot_intent_ranking, weights):
-        """
-        Computes the weighted average of the confidence scores from the DIET and Zero-Shot classifiers.
-
-        :param diet_intent_ranking: The intent ranking from the DIET classifier.
-        :param zero_shot_intent_ranking: The intent ranking from the Zero-Shot classifier.
-        :param weights: The weights assigned to the DIET and Zero-Shot classifiers.
-        :return: The intent ranking with confidence scores computed as the weighted average.
-        """
-        diet_confidences = {intent['name']: intent['confidence'] for intent in diet_intent_ranking}
-        zero_shot_confidences = {intent['name']: intent['confidence'] for intent in zero_shot_intent_ranking}
-
-        averaged_confidences = {}
-
-        # Compute the weighted average for each intent.
-        for intent in set(diet_confidences.keys()).union(zero_shot_confidences.keys()):
-            diet_confidence = diet_confidences.get(intent, 0)
-            zero_shot_confidence = zero_shot_confidences.get(intent, 0)
-            averaged_confidences[intent] = diet_confidence * weights[0] + zero_shot_confidence * weights[1]
-
-        # Transform the dictionary back into a list of dictionaries.
-        averaged_intent_ranking = [{'name': intent, 'confidence': confidence} for intent, confidence in
-                                   averaged_confidences.items()]
-
-        # Sort the intents by their confidence score in descending order.
-        averaged_intent_ranking.sort(key=lambda intent: -intent['confidence'])
-
-        return averaged_intent_ranking
-
-        # TODO: USAGE
-        # weighted_intent_ranking = self.compute_weighted_average(
-        #     intent_ranking, new_intent_ranking,[0.5, 0.5])  # Adjust the weights as needed.
-        # message.set(INTENT, weighted_intent_ranking[0], add_to_output=True)
-        # message.set(INTENT_RANKING_KEY, weighted_intent_ranking, add_to_output=True)
-
-    # TODO: test Softmax scoring method
-    @staticmethod
-    def softmax(x):
-        """Compute softmax values for each sets of scores in x."""
-        e_x = np.exp(x - np.max(x))
-        return e_x / e_x.sum()
-
-    @staticmethod
-    def postprocess_confidence_scores(self, intent_ranking):
-        # Convert the confidence scores to a numpy array
-        confidences = np.array([intent['confidence'] for intent in intent_ranking])
-
-        # Apply the softmax function
-        softmax_confidences = self.softmax(confidences)
-
-        # Update the confidence scores in the intent ranking
-        for i, intent in enumerate(intent_ranking):
-            intent['confidence'] = softmax_confidences[i]
-
-        return intent_ranking
+    # Unused methods
+    #
+    # @staticmethod
+    # def compute_weighted_average(diet_intent_ranking, zero_shot_intent_ranking, weights):
+    #     """
+    #     Computes the weighted average of the confidence scores from the DIET and Zero-Shot classifiers.
+    #
+    #     :param diet_intent_ranking: The intent ranking from the DIET classifier.
+    #     :param zero_shot_intent_ranking: The intent ranking from the Zero-Shot classifier.
+    #     :param weights: The weights assigned to the DIET and Zero-Shot classifiers.
+    #     :return: The intent ranking with confidence scores computed as the weighted average.
+    #     """
+    #     diet_confidences = {intent['name']: intent['confidence'] for intent in diet_intent_ranking}
+    #     zero_shot_confidences = {intent['name']: intent['confidence'] for intent in zero_shot_intent_ranking}
+    #
+    #     averaged_confidences = {}
+    #
+    #     # Compute the weighted average for each intent.
+    #     for intent in set(diet_confidences.keys()).union(zero_shot_confidences.keys()):
+    #         diet_confidence = diet_confidences.get(intent, 0)
+    #         zero_shot_confidence = zero_shot_confidences.get(intent, 0)
+    #         averaged_confidences[intent] = diet_confidence * weights[0] + zero_shot_confidence * weights[1]
+    #
+    #     # Transform the dictionary back into a list of dictionaries.
+    #     averaged_intent_ranking = [{'name': intent, 'confidence': confidence} for intent, confidence in
+    #                                averaged_confidences.items()]
+    #
+    #     # Sort the intents by their confidence score in descending order.
+    #     averaged_intent_ranking.sort(key=lambda intent: -intent['confidence'])
+    #
+    #     return averaged_intent_ranking
+    #
+    #     # TODO: USAGE
+    #     # weighted_intent_ranking = self.compute_weighted_average(
+    #     #     intent_ranking, new_intent_ranking,[0.5, 0.5])  # Adjust the weights as needed.
+    #     # message.set(INTENT, weighted_intent_ranking[0], add_to_output=True)
+    #     # message.set(INTENT_RANKING_KEY, weighted_intent_ranking, add_to_output=True)
+    #
+    # # TODO: test Softmax scoring method
+    # @staticmethod
+    # def softmax(x):
+    #     """Compute softmax values for each sets of scores in x."""
+    #     e_x = np.exp(x - np.max(x))
+    #     return e_x / e_x.sum()
+    #
+    # @staticmethod
+    # def postprocess_confidence_scores(self, intent_ranking):
+    #     # Convert the confidence scores to a numpy array
+    #     confidences = np.array([intent['confidence'] for intent in intent_ranking])
+    #
+    #     # Apply the softmax function
+    #     softmax_confidences = self.softmax(confidences)
+    #
+    #     # Update the confidence scores in the intent ranking
+    #     for i, intent in enumerate(intent_ranking):
+    #         intent['confidence'] = softmax_confidences[i]
+    #
+    #     return intent_ranking
