9320dc92398b3cbbeca6584c91f1a9b37ec881a6
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-06-03 00:29:37 +0530

    updated nlu data with responses of google form

--- submodules/custom_components/intent_classifiers/zero_shot_bart_intent_classifier.py
+++ submodules/custom_components/intent_classifiers/zero_shot_bart_intent_classifier.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 
 @DefaultV1Recipe.register(
-    DefaultV1Recipe.ComponentType.INTENT_CLASSIFIER, is_trainable=False
+    DefaultV1Recipe.ComponentType.INTENT_CLASSIFIER, is_trainable=True
 )
 class ZeroShotBartIntentClassifier(GraphComponent):
     """A GraphComponent in RASA for Intent classification using a ZeroShot BART model.
