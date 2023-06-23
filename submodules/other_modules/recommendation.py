59ecb0db1a8ca11d88245ae36a26841d7d7f20c4
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-06-23 17:27:26 +0530

    recommendation module files refactored

--- submodules/other_modules/recommendation.py
+++ submodules/other_modules/recommendation.py
@@ -1,42 +1,41 @@
-
-import pandas as pd
-import numpy as np
-from textblob import TextBlob
-from sklearn.metrics.pairwise import cosine_similarity
-
 import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from nltk.tag import pos_tag
+from nltk.tokenize import sent_tokenize, word_tokenize
+from sklearn.metrics.pairwise import cosine_similarity
+from textblob import TextBlob
 
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 
+
 def get_feature_words():
     return {
-    'feature 1' :  ['service', 'experience', 'look', 'flavor', 'star', 'dish', 'store', 'tasty', 'come', 'return'],
-    'feature 2' :  ['order', 'lunch' ,'dinner', 'enjoy', 'friend', 'breakfast', 'onion', 'soup','point', 'town'],
-    'feature 3' :  ['eat', 'way', 'option', 'feel', 'portion', 'bowl', 'start', 'house', 'size','think'],
-    'feature 4' :  ['visit', 'staff', 'night', 'serve', 'work', 'chip', 'change', 'let', 'counter','taste'],
-    'feature 5' :  ['chicken', 'fry', 'sandwich', 'roll', 'bread', 'tender', 'make', 'shrimp','appetizer', 'grill'],
-    'feature 6' :  ['time', 'year', 'spot', 'bit', 'couple', 'use', 'water', 'thank', 'treat', 'dog'],
-    'feature 7' :  ['location', 'item', 'space', 'quality', 'share', 'stuff', 'plan', 'menu','favorite', 'trip'],
-    'feature 8' :  ['place', 'day', 'wait', 'minute', 'choice', 'delivery', 'crispy', 'pay', 'tea','bite'],
-    'feature 9' :  ['pizza', 'review', 'beer', 'parking', 'style', 'park', 'stop', 'street', 'plenty','walk'],
-    'feature 10' :  ['restaurant', 'home', 'rice', 'ask', 'pork', 'need', 'dine', 'customer', 'pack','wine'],
-    'feature 11' :  ['food', 'thing', 'lot', 'plate', 'cup', 'truck', 'taste', 'chain', 'juice','protein'],
-    'feature 12' :  ['menu', 'meal', 'people', 'price', 'salad', 'potato', 'business', 'head','ingredient', 'reason'],
-    'feature 13' :  ['seat', 'hour', 'pick', 'piece', 'half', 'person', 'course', 'grab', 'deal','note'],
-    'feature 14' :  ['bar', 'table', 'drink', 'sit', 'cook', 'fun', 'tell', 'steak', 'honey', 'cold'],
-    'feature 15' :  ['try', 'love', 'area', 'room', 'dessert', 'kind', 'reservation', 'locate', 'miss','manager'],
-    'feature 16' :  ['cheese', 'end', 'slice', 'husband', 'bring', 'pie', 'crust', 'sweet', 'base','group'],
-    'feature 17' :  ['coffee', 'egg', 'bacon', 'tomato', 'cake', 'mushroom', 'pepper', 'sausage','pickle', 'mix'],
-    'feature 18' :  ['sauce', 'drive', 'family', 'door', 'today', 'touch', 'heat', 'distance', 'boy','face'],
-    'feature 19' :  ['meat', 'line', 'beef', 'bun', 'wall', 'morning', 'establishment', 'phone', 'pub','stand'],
-    'feature 20' :  ['spicy', 'hand', 'mask', 'super', 'car', 'window', 'purchase', 'toast', 'kitchen','sign'],
+        'feature 1': ['service', 'experience', 'look', 'flavor', 'star', 'dish', 'store', 'tasty', 'come', 'return'],
+        'feature 2': ['order', 'lunch', 'dinner', 'enjoy', 'friend', 'breakfast', 'onion', 'soup', 'point', 'town'],
+        'feature 3': ['eat', 'way', 'option', 'feel', 'portion', 'bowl', 'start', 'house', 'size', 'think'],
+        'feature 4': ['visit', 'staff', 'night', 'serve', 'work', 'chip', 'change', 'let', 'counter', 'taste'],
+        'feature 5': ['chicken', 'fry', 'sandwich', 'roll', 'bread', 'tender', 'make', 'shrimp', 'appetizer', 'grill'],
+        'feature 6': ['time', 'year', 'spot', 'bit', 'couple', 'use', 'water', 'thank', 'treat', 'dog'],
+        'feature 7': ['location', 'item', 'space', 'quality', 'share', 'stuff', 'plan', 'menu', 'favorite', 'trip'],
+        'feature 8': ['place', 'day', 'wait', 'minute', 'choice', 'delivery', 'crispy', 'pay', 'tea', 'bite'],
+        'feature 9': ['pizza', 'review', 'beer', 'parking', 'style', 'park', 'stop', 'street', 'plenty', 'walk'],
+        'feature 10': ['restaurant', 'home', 'rice', 'ask', 'pork', 'need', 'dine', 'customer', 'pack', 'wine'],
+        'feature 11': ['food', 'thing', 'lot', 'plate', 'cup', 'truck', 'taste', 'chain', 'juice', 'protein'],
+        'feature 12': ['menu', 'meal', 'people', 'price', 'salad', 'potato', 'business', 'head', 'ingredient',
+                       'reason'],
+        'feature 13': ['seat', 'hour', 'pick', 'piece', 'half', 'person', 'course', 'grab', 'deal', 'note'],
+        'feature 14': ['bar', 'table', 'drink', 'sit', 'cook', 'fun', 'tell', 'steak', 'honey', 'cold'],
+        'feature 15': ['try', 'love', 'area', 'room', 'dessert', 'kind', 'reservation', 'locate', 'miss', 'manager'],
+        'feature 16': ['cheese', 'end', 'slice', 'husband', 'bring', 'pie', 'crust', 'sweet', 'base', 'group'],
+        'feature 17': ['coffee', 'egg', 'bacon', 'tomato', 'cake', 'mushroom', 'pepper', 'sausage', 'pickle', 'mix'],
+        'feature 18': ['sauce', 'drive', 'family', 'door', 'today', 'touch', 'heat', 'distance', 'boy', 'face'],
+        'feature 19': ['meat', 'line', 'beef', 'bun', 'wall', 'morning', 'establishment', 'phone', 'pub', 'stand'],
+        'feature 20': ['spicy', 'hand', 'mask', 'super', 'car', 'window', 'purchase', 'toast', 'kitchen', 'sign'],
     }
+
 
 def extract_sentences_with_features(text, feature_words):
     stopwords_set = set(stopwords.words('english'))
@@ -59,10 +58,12 @@ def extract_sentences_with_features(text, feature_words):
     for feature_word, related_words in feature_words.items():
         relevant_sentences[feature_word] = []
         for sentence_tokens in preprocessed_text:
-            if any((feature_word in sentence_tokens) or (related_word in sentence_tokens) for related_word in related_words):
+            if any((feature_word in sentence_tokens) or (related_word in sentence_tokens) for related_word in
+                   related_words):
                 relevant_sentences[feature_word].append(' '.join(sentence_tokens))
 
     return relevant_sentences
+
 
 def get_feature_df(df, feature_words):
     df['feature_summaries'] = df['review'].apply(lambda x: extract_sentences_with_features(x, feature_words))
@@ -77,9 +78,11 @@ def get_feature_df(df, feature_words):
 
     return df
 
+
 def calculate_sentiment_score(sentence):
     blob = TextBlob(sentence)
     return blob.sentiment.polarity
+
 
 def get_rec_dfs(df_sent):
     df_user_res_grp = df_sent.groupby(["business_id", "user_id"]).apply(lambda x: x[x != 0].mean(numeric_only=True))
@@ -97,6 +100,7 @@ def get_rec_dfs(df_sent):
     df_res_grp.fillna(0.0, inplace=True)
 
     return df_user_res_grp, df_user_grp, df_res_grp
+
 
 def collaborative_filtering(target_user, df_user_res_grp, df_user_grp, features, similarity_threshold=0.7):
     selected_user_df = df_user_grp[df_user_grp['user_id'] == target_user]
@@ -123,21 +127,26 @@ def collaborative_filtering(target_user, df_user_res_grp, df_user_grp, features,
 
     df_user_res_wt_sim = pd.merge(df_user_res_grp, similar_users, on='user_id')
     df_user_res_wt_sim = df_user_res_wt_sim.drop(columns=sel_usr_zro_cols)
-    df_user_res_wt_sim[sel_usr_features] = df_user_res_wt_sim[sel_usr_features].mul(df_user_res_wt_sim['similarity_score'], axis=0)
+    df_user_res_wt_sim[sel_usr_features] = df_user_res_wt_sim[sel_usr_features].mul(
+        df_user_res_wt_sim['similarity_score'], axis=0)
 
-    aggregated_ratings = df_user_res_wt_sim[df_user_res_wt_sim['user_id'].isin(filtered_user_ids)].groupby('business_id')[sel_usr_features].apply(lambda x: x[x != 0].mean())
+    aggregated_ratings = \
+    df_user_res_wt_sim[df_user_res_wt_sim['user_id'].isin(filtered_user_ids)].groupby('business_id')[
+        sel_usr_features].apply(lambda x: x[x != 0].mean())
     aggregated_ratings.fillna(0.0, inplace=True)
 
     predicted_ratings_scr = aggregated_ratings.apply(lambda x: x[x != 0].mean(), axis=1)
     predicted_ratings_scr = predicted_ratings_scr.dropna()
 
-    normalized_ratings = (predicted_ratings_scr - predicted_ratings_scr.min()) / (predicted_ratings_scr.max() - predicted_ratings_scr.min()) * 4 + 1
+    normalized_ratings = (predicted_ratings_scr - predicted_ratings_scr.min()) / (
+                predicted_ratings_scr.max() - predicted_ratings_scr.min()) * 4 + 1
 
     all_businesses = normalized_ratings.sort_values(ascending=False)
 
     df_cf = pd.DataFrame({'business_id': all_businesses.index, 'cf_predicted_rating': all_businesses.values})
 
     return df_cf
+
 
 def content_based_filtering(df_user_grp, df_res_grp, target_user):
     selected_user_preferences = df_user_grp.loc[df_user_grp['user_id'] == target_user].drop('user_id', axis=1)
@@ -159,16 +168,21 @@ def content_based_filtering(df_user_grp, df_res_grp, target_user):
 
     return df_cbf
 
+
 def hybrid_recommendation(colab_df, content_df, content_weight, collab_filtering_weight, n_recommendations):
     hy_df = pd.merge(content_df, colab_df, on='business_id')
 
-    hy_df['weighted_hybrid_score'] = hy_df['cbf_predicted_rating'] * content_weight + hy_df['cf_predicted_rating'] * collab_filtering_weight
+    hy_df['weighted_hybrid_score'] = hy_df['cbf_predicted_rating'] * content_weight + hy_df[
+        'cf_predicted_rating'] * collab_filtering_weight
 
-    ranked_business_ids = hy_df.sort_values(by='weighted_hybrid_score', ascending=False)['business_id'].head(n_recommendations).tolist()
+    ranked_business_ids = hy_df.sort_values(by='weighted_hybrid_score', ascending=False)['business_id'].head(
+        n_recommendations).tolist()
     return ranked_business_ids
 
+
 def recommendation(user_id, df, n_restaurants=10):
-    df = df.drop(columns=['categories','address', 'state_', 'city', 'postal_code', 'latitude', 'longitude', 'stars', 'review_count', 'is_open', 'hours', 'review_id', 'useful', 'funny', 'cool', 'date_', 'name' ])
+    df = df.drop(columns=['categories', 'address', 'state_', 'city', 'postal_code', 'latitude', 'longitude', 'stars',
+                          'review_count', 'is_open', 'hours', 'review_id', 'useful', 'funny', 'cool', 'date_', 'name'])
     df = df.rename(columns={'text_': 'review'})
     df = df.dropna()
     df = df.drop_duplicates()
@@ -179,20 +193,24 @@ def recommendation(user_id, df, n_restaurants=10):
 
     feature_name_list = list(df_sent['feature_summaries'][0].keys())
 
-    df_sent["sentiment_scores"] = df_sent["feature_summaries"].apply(lambda row: {feature: calculate_sentiment_score(sentence) for feature, sentence in row.items()})
+    df_sent["sentiment_scores"] = df_sent["feature_summaries"].apply(
+        lambda row: {feature: calculate_sentiment_score(sentence) for feature, sentence in row.items()})
     sentiment_scores_df = pd.DataFrame(df_sent["sentiment_scores"].tolist())
     df_sent = pd.concat([df_sent, sentiment_scores_df], axis=1)
-    df_sent = df_sent.drop(columns=[ 'review', 'feature_summaries', 'sentiment_scores'])
+    df_sent = df_sent.drop(columns=['review', 'feature_summaries', 'sentiment_scores'])
 
     df_user_res_feature_scr, df_user_feature_scr, df_res_feature_scr = get_rec_dfs(df_sent)
 
-    cf_df = collaborative_filtering(target_user=user_id, df_user_res_grp=df_user_res_feature_scr, df_user_grp=df_user_feature_scr, features=feature_name_list, similarity_threshold=0.7)
-    cbf_df = content_based_filtering(df_user_grp=df_user_feature_scr, df_res_grp=df_res_feature_scr, target_user=user_id)
+    cf_df = collaborative_filtering(target_user=user_id, df_user_res_grp=df_user_res_feature_scr,
+                                    df_user_grp=df_user_feature_scr, features=feature_name_list,
+                                    similarity_threshold=0.7)
+    cbf_df = content_based_filtering(df_user_grp=df_user_feature_scr, df_res_grp=df_res_feature_scr,
+                                     target_user=user_id)
 
-    ranked_rest_ids = hybrid_recommendation(colab_df=cf_df, content_df=cbf_df, content_weight=0.5, collab_filtering_weight=0.5, n_recommendations=n_restaurants)
+    ranked_rest_ids = hybrid_recommendation(colab_df=cf_df, content_df=cbf_df, content_weight=0.5,
+                                            collab_filtering_weight=0.5, n_recommendations=n_restaurants)
 
     return ranked_rest_ids
-
 
 
 # Run this
